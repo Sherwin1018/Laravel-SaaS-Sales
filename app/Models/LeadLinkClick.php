53599eef03908be9facf07534cd5c7df85c500f3
basillote-angel <angelrosebasillote888@gmail.com>
2026-03-27 16:22:11 +0800

    Add workflow/sequence tracking to LeadLinkClick for automation analytics

--- app/Models/LeadLinkClick.php
+++ app/Models/LeadLinkClick.php
@@ -7,6 +7,12 @@ class LeadLinkClick extends Model
     protected $fillable = [
         'tenant_id',
         'lead_id',
+        'workflow_id',
+        'sequence_id',
+        'sequence_step_order',
+        'link_name',
+        'destination_url',
+        'click_number',
         'clicked_at',
     ];
 }
